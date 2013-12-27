756a06258efab350428d20451cf24e8092c475f5
Bobirkhon Ismailov <bismailov@gmail.com>
2013-12-27 22:10:56 +0500

    On branch master 	deleted:    db/migrate/20131211145416_remove_description_from_student_levels.rb

--- db/migrate/20131211145416_remove_description_from_student_levels.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDescriptionFromStudentLevels < ActiveRecord::Migration
-  def up
-    remove_column :student_levels, :description
-  end
-
-  def down
-    add_column :student_levels, :description, :string
-  end
-end
